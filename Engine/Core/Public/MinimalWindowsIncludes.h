47cc32f04b839145683629dd1b301d908750a59c
Matt <144626797+1mB33@users.noreply.github.com>
2026-01-20 23:10:44 +0100

    Update MinimalWindowsIncludes.h

--- Engine/Core/Public/MinimalWindowsIncludes.h
+++ Engine/Core/Public/MinimalWindowsIncludes.h
@@ -2,6 +2,7 @@
 #   ifndef AB_MINIMAL_WINDOWS_INLCUDES_H
 #   define AB_MINIMAL_WINDOWS_INLCUDES_H
 #      define WIN_LEAN_AND_MEAN
+#      define NOMINMAX
 #      include <Windows.h>
 #      include <winDNS.h>
 #      include <windowsx.h>   
