1bd3d49970552af599ada5915fa7cb7ea677231d
Im-Bee <im.bee.vibe@gmail.com>
2026-04-16 22:15:47 +0200

    refact: ComputerAdapter -> GraphicsComputeAdapter

---
+++ Engine/Rendering/Private/Vulkan/GraphicsComputeAdapter.cpp
@@ -0,0 +1,22 @@
+#include "B33Rendering.hpp"
+
+#include "Vulkan/GraphicsComputeAdapter.hpp"
+
+namespace B33::Rendering
+{
+
+using namespace std;
+
+// --------------------------------------------------------------------------------------------------------------------
+const vector<const char *> &GraphicsComputeAdapter::GetExtensionsImpl() const
+{
+    return m_vExtensions;
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+void *GraphicsComputeAdapter::GetFeaturesImpl() const
+{
+    return NULL;
+}
+
+}; // namespace B33::Rendering
